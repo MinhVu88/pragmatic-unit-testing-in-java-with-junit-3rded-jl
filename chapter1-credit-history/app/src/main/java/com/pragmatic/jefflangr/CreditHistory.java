a3e4328f4cdc013245888e74cb68214ecec5872c
MinhVu88 <minhvu72826@gmail.com>
2025-10-26 16:54:38 +0700

    Oct 26th 25 - 17:00

--- chapter1-credit-history/app/src/main/java/com/pragmatic/jefflangr/CreditHistory.java
+++ chapter1-credit-history/app/src/main/java/com/pragmatic/jefflangr/CreditHistory.java
@@ -6,12 +6,16 @@ import java.util.List;
 public class CreditHistory {
 	private final List<CreditRating> ratings = new ArrayList<>();
 
-	public void collect(CreditRating score) {
-		ratings.add(score);
+	public void collect(CreditRating rating) {
+		this.ratings.add(rating);
 	}
 
 	public int getArithmeticMean() {
-		var total = ratings.stream().mapToInt(CreditRating::score).sum();
-		return total / ratings.size();
+		if(this.ratings.isEmpty()) {
+			return 0;
+		}
+
+		var total = this.ratings.stream().mapToInt(CreditRating::rating).sum();
+		return total / this.ratings.size();
 	}
 }
--- chapter1-credit-history/app/src/main/java/com/pragmatic/jefflangr/CreditRating.java
+++ chapter1-credit-history/app/src/main/java/com/pragmatic/jefflangr/CreditRating.java
@@ -1,3 +1,3 @@
 package com.pragmatic.jefflangr;
 
-public record CreditRating(int score) {}
+public record CreditRating(int rating) {}
